b51950333fa5d95cb22a476caa836867c3c152a3
Tales Viegas <tales.viegas@ilegra.com>
2018-11-22 21:33:00 -0200

    Validate election with votes

--- src/main/java/br/edu/ulbra/election/voter/VoterApplication.java
+++ src/main/java/br/edu/ulbra/election/voter/VoterApplication.java
@@ -2,8 +2,10 @@ package br.edu.ulbra.election.voter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VoterApplication {
 
 	public static void main(String[] args) {
